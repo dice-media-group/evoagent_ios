270b6436d2d3d81b9072b4a41c75e3bf6e88be20
Carl Tanner <rocketeer.captproton@gmail.com>
2017-03-08 13:08:25 -0800

    Initial Commit

---
+++ EVO Agent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EVO Agent
+//
+//  Created by Captain Proton on 3/8/17.
+//  Copyright © 2017 dicemediagroup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
